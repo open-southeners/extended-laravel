6e19f94a7104cbcfb8f79248afa2e6ad81928af8
Rubén Robles <me@d8vjork.com>
2025-02-16 02:58:33 +0100

    add Number::toByteUnit macro

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - `Collection::templateJoin` macro
+- `Number::toByteUnit` macro
 
 ## [0.5.0] - 2025-01-24
 
--- src/Support/Number.php
+++ src/Support/Number.php
@@ -3,6 +3,7 @@
 namespace OpenSoutheners\ExtendedLaravel\Support;
 
 use Closure;
+use OpenSoutheners\ByteUnitConverter\ByteUnitConverter;
 
 use function OpenSoutheners\ExtendedPhp\Numbers\short_number;
 
@@ -20,5 +21,13 @@ class Number
          * Get shorter version of a big number if possible.
          */
         return fn (int|float $number): string => short_number($number);
+    }
+
+    public function toByteUnit(): Closure
+    {
+        /**
+         * Get byte unit version of a number.
+         */
+        return fn (int|string $number): ByteUnitConverter => ByteUnitConverter::new($number);
     }
 }
