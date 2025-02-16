c3c13ac46ffaa72f737ef6a2f03f85f04ea4db31
Rubén Robles <me@d8vjork.com>
2025-02-16 02:11:40 +0100

    add Collection::templateJoin macro

--- src/Support/Collection.php
+++ src/Support/Collection.php
@@ -3,6 +3,9 @@
 namespace OpenSoutheners\ExtendedLaravel\Support;
 
 use Closure;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /**
  * This is NOT supposed to be used alone, use the main from Laravel framework
@@ -25,6 +28,37 @@ class Collection
             $csvContent .= implode("\n", $this->map(fn ($result) => implode(',', array_values((array) $result)))->toArray());
 
             return $csvContent;
+        };
+    }
+
+    public function templateJoin()
+    {
+        /**
+         * Join items of the collection using a template substitution.
+         */
+        return function (string $template, string $glue, string $finalGlue = ''): string {
+            $items = $this->items;
+
+            if (Arr::isAssoc($items)) {
+                $items = [$items];
+            }
+
+            $items = Arr::map($items, function ($item) use ($template): string {
+                $sanitizedItem = array_filter(
+                    $item instanceof Arrayable ? $item->toArray() : $item,
+                    fn ($value): bool => ! is_array($value) && ! is_object($value) && ! is_callable($value)
+                );
+
+                $replacements = Arr::map(array_keys($sanitizedItem), fn (string $key): string => ":{$key}");
+
+                return Str::replace($replacements, array_values($sanitizedItem), $template);
+            });
+
+            if ($finalGlue === '') {
+                return implode($glue, $items);
+            }
+
+            return Arr::join($items, $glue, $finalGlue);
         };
     }
 }
